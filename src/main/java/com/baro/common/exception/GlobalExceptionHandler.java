b243fac400830f68062879d4c7b30e7d0d152ba9
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-12-08 23:35:28 +0900

    feat: RequestException과 Server 오류 핸들러 추가

--- src/main/java/com/baro/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/baro/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.baro.common.exception;
 
+import com.baro.common.response.ExceptionResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,9 +12,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 class GlobalExceptionHandler {
 
     @ExceptionHandler(value = {RequestException.class})
-    protected ResponseEntity<?> handleBadRequestException(RequestException e) {
-        log.error("handleBadRequestException throw BadRequestException : {}", e.getMessage());
-        return ApiResponse.badRequest(e.getMessage(), null);
+    protected ResponseEntity<ExceptionResponse<?>> handleBadRequestException(RequestException e) {
+        log.warn("[handleBadRequestException throw BadRequestException : {}", e.getMessage());
+        RequestExceptionType exceptionType = e.exceptionType();
+        return ResponseEntity.status(exceptionType.httpStatus())
+                .body(new ExceptionResponse<>(exceptionType));
     }
 
+    @ExceptionHandler(Exception.class)
+    ResponseEntity<String> handleException(HttpServletRequest request, Exception e) {
+        log.error("[ERROR] Unexpected error occurred. FROM URI: {}, ", request.getRequestURI(), e);
+        return ResponseEntity.internalServerError()
+                .body("Unknown error occurred. Please try later");
+    }
 }
