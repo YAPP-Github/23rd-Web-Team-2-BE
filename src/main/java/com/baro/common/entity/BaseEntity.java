4bd023967d1fbd29b5add48d567f6adf8b6b1f38
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-12-24 15:30:50 +0900

    feat: Id와 생성일자 필드를 포함하는 BaseEntity 추가

---
+++ src/main/java/com/baro/common/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.baro.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false, columnDefinition = "BIGINT UNSIGNED")
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
