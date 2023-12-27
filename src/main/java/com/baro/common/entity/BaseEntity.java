df5dd334ddac224f3c183da5448e6d162c9f5554
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-12-27 16:31:51 +0900

    feat: 마지막 수정 날짜 필드 추가

--- src/main/java/com/baro/common/entity/BaseEntity.java
+++ src/main/java/com/baro/common/entity/BaseEntity.java
@@ -9,6 +9,7 @@ import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
@@ -23,4 +24,7 @@ public class BaseEntity {
 
     @CreatedDate
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
