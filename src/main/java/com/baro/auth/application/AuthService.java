0f8afc84309c6e154452a5d9db1ac317ca2ea8d2
shinyubin989 <dbqls2821@gmail.com>
2024-01-03 02:18:31 +0900

    feat: AuthService, AuthController 의존성, 로직 정의

---
+++ src/main/java/com/baro/auth/application/AuthService.java
@@ -0,0 +1,30 @@
+package com.baro.auth.application;
+
+import com.baro.auth.domain.Token;
+import com.baro.member.application.MemberCreator;
+import com.baro.member.domain.Member;
+import com.baro.member.domain.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class AuthService {
+
+    private final MemberRepository memberRepository;
+    private final MemberCreator memberCreator;
+    private final TokenProvider tokenProvider;
+
+    public Token signIn(String name, String email, String oauthId, String oauthType) {
+        Member member = memberRepository.findByOAuthIdAndOAuthServiceType(oauthId, oauthType)
+                .orElseGet(() -> memberCreator.create(name, email, oauthId, oauthType));
+
+        return tokenProvider.provide(member);
+    }
+
+    public void signOut(String token) {
+        // TODO
+    }
+}
