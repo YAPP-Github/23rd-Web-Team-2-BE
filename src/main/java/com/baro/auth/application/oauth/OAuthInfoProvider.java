3d140a433dfcd331c4cf7bc1abfa0c737d84bec7
shinyubin989 <dbqls2821@gmail.com>
2024-01-03 02:12:11 +0900

    feat: oauth client를 이용한 응답값을 전달해주는 OAuthInfoProvider 추가

---
+++ src/main/java/com/baro/auth/application/oauth/OAuthInfoProvider.java
@@ -0,0 +1,41 @@
+package com.baro.auth.application.oauth;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
+
+import com.baro.auth.application.oauth.dto.OAuthMemberInfo;
+import com.baro.auth.application.oauth.dto.OAuthTokenInfo;
+import com.baro.auth.domain.oauth.OAuthServiceType;
+
+import java.util.Map;
+import java.util.Set;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class OAuthInfoProvider {
+
+    private final Map<OAuthServiceType, OAuthClient> clients;
+
+    public OAuthInfoProvider(Set<OAuthClient> clients) {
+        this.clients = clients.stream()
+                .collect(toMap(OAuthClient::getOAuthService, identity()));
+    }
+
+    @Transactional(readOnly = true)
+    public String getSignInUrl(String oAuthService) {
+        OAuthClient client = getClient(oAuthService);
+        return client.getSignInUrl();
+    }
+
+    public OAuthMemberInfo getMemberInfo(String oAuthService, String code) {
+        OAuthClient client = getClient(oAuthService);
+        OAuthTokenInfo oAuthTokenInfo = client.requestAccessToken(code);
+        return client.requestMemberInfo(oAuthTokenInfo);
+    }
+
+    private OAuthClient getClient(String oauthType) {
+        OAuthServiceType oAuthServiceType = OAuthServiceType.from(oauthType);
+        return clients.get(oAuthServiceType);
+    }
+}
