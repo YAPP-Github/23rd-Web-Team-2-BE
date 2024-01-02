1b09a05fcb491f2fd7967c67dbfc44810cdcad66
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-01-02 22:07:59 +0900

    feat: S3 이미지 업로더 구현

--- src/main/java/com/baro/common/infra/aws/s3/AwsImageUploader.java
+++ src/main/java/com/baro/common/infra/aws/s3/AwsImageUploader.java
@@ -1,12 +1,44 @@
 package com.baro.common.infra.aws.s3;
 
 import com.baro.common.client.ImageUploader;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+@RequiredArgsConstructor
+@Component
 public class AwsImageUploader implements ImageUploader {
 
+    private final S3Client s3Client;
+    private final AwsS3Property awsS3Property;
+
     @Override
-    public void upload(final MultipartFile file) {
-        
+    public void upload(MultipartFile file) {
+        try {
+            String urlSafetyKey = changeToUrlSafetyKey(file);
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(awsS3Property.bucketName())
+                    .key(awsS3Property.key() + urlSafetyKey)
+                    .contentType(file.getContentType())
+                    .contentDisposition("inline")
+                    .build();
+
+            RequestBody requestBody = RequestBody.fromInputStream(file.getInputStream(), file.getSize());
+            s3Client.putObject(putObjectRequest, requestBody);
+        } catch (IOException ioException) {
+            throw new RuntimeException(ioException); //TODO: custom exception 으로 수정
+        }
+    }
+
+    private String changeToUrlSafetyKey(MultipartFile file) {
+        String originalFilename = file.getOriginalFilename();
+        String replacedFileName = Objects.requireNonNull(originalFilename).replace(" ", "_");
+        return replacedFileName + "-" + UUID.randomUUID();
     }
 }
