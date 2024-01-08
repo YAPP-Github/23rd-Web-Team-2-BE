dc49ccf745f40ccc18c3015ec8990dee872f75d2
shinyubin989 <dbqls2821@gmail.com>
2024-01-09 01:13:48 +0900

    refactor: ip header 타입 "X-Forwarded-For"로 단일화

--- src/main/java/com/baro/auth/presentation/RequestResolver.java
+++ src/main/java/com/baro/auth/presentation/RequestResolver.java
@@ -3,8 +3,6 @@ package com.baro.auth.presentation;
 import com.baro.auth.exception.AuthException;
 import com.baro.auth.exception.AuthExceptionType;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -19,34 +17,13 @@ public class RequestResolver {
         if(requestAttributes == null) throw new AuthException(AuthExceptionType.IP_ADDRESS_DOES_NOT_EXIST);
 
         HttpServletRequest request = requestAttributes.getRequest();
-        for (IpHeaderCandidates ipHeaderCandidate : IpHeaderCandidates.values()) {
-            String ip = request.getHeader(ipHeaderCandidate.getName());
-            if (ip != null && !ip.isEmpty() && !"unknown".equalsIgnoreCase(ip)) {
-                String s = ip.split(",")[0];
-                log.info("RequestResolver::extractIp -> {}", s);
-                return s;
-            }
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip != null && !ip.isEmpty() && !"unknown".equalsIgnoreCase(ip)) {
+            String requestIp = ip.split(",")[0];
+            log.info("RequestResolver::extractIp -> {}", requestIp);
+            return requestIp;
         }
         log.info("RequestResolver::extractIp -> {}", request.getRemoteAddr());
         return request.getRemoteAddr();
-    }
-
-    @RequiredArgsConstructor
-    @Getter
-    enum IpHeaderCandidates {
-        X_FORWARDED_FOR("X-Forwarded-For"),
-        PROXY_CLIENT_IP("Proxy-Client-IP"),
-        WL_PROXY_CLIENT_IP("WL-Proxy-Client-IP"),
-        HTTP_CLIENT_IP("HTTP_CLIENT_IP"),
-        HTTP_X_FORWARDED_FOR("HTTP_X_FORWARDED_FOR"),
-        HTTP_X_FORWARDED("HTTP_X_FORWARDED"),
-        HTTP_X_CLUSTER_CLIENT_IP("HTTP_X_CLUSTER_CLIENT_IP"),
-        HTTP_FORWARDED_FOR("HTTP_FORWARDED_FOR"),
-        HTTP_FORWARDED("HTTP_FORWARDED"),
-        HTTP_VIA("HTTP_VIA"),
-        REMOTE_ADDR("REMOTE_ADDR"),
-        ;
-
-        private final String name;
     }
 }
