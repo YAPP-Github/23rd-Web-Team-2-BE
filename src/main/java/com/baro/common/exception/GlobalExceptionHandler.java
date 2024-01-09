f812d53addecadb6a95f1b1767199715c0940409
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-01-09 09:43:11 +0900

    feat: 에러 코드를 포함하는 Exception Response 추가

--- src/main/java/com/baro/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/baro/common/exception/GlobalExceptionHandler.java
@@ -13,21 +13,24 @@ import org.springframework.web.multipart.MaxUploadSizeExceededException;
 class GlobalExceptionHandler {
 
     @ExceptionHandler(value = {RequestException.class})
-    ResponseEntity<String> handleBadRequestException(RequestException e) {
+    ResponseEntity<RequestExceptionResponse> handleBadRequestException(RequestException e) {
         log.warn("[handleRequestException throw RequestException : {}", e.getMessage());
         RequestExceptionType exceptionType = e.exceptionType();
-        return ResponseEntity.status(exceptionType.httpStatus()).body(exceptionType.errorMessage());
+        return ResponseEntity.status(exceptionType.httpStatus())
+                .body(RequestExceptionResponse.from(exceptionType));
     }
 
     @ExceptionHandler(value = {MaxUploadSizeExceededException.class})
     public ResponseEntity<String> fileSizeLimitExceeded(MaxUploadSizeExceededException e) {
         log.warn("[handleRequestException throw MaxUploadSizeExceededException : {}", e.getMessage());
-        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body("File size limit exceeded");
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
+                .body("File size limit exceeded");
     }
 
     @ExceptionHandler(Exception.class)
     ResponseEntity<String> handleException(HttpServletRequest request, Exception e) {
         log.error("[ERROR] Unexpected error occurred. FROM URI: {}, ", request.getRequestURI(), e);
-        return ResponseEntity.internalServerError().body("Unknown error occurred. Please try later");
+        return ResponseEntity.internalServerError()
+                .body("Unknown error occurred. Please try later");
     }
 }
