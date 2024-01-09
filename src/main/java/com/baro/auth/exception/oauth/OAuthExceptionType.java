c258b550f98d2d9bc25b1ff1a6b6298d6c73ca7e
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-01-09 09:38:11 +0900

    feat: 에러 코드 추가

--- src/main/java/com/baro/auth/exception/oauth/OAuthExceptionType.java
+++ src/main/java/com/baro/auth/exception/oauth/OAuthExceptionType.java
@@ -7,11 +7,17 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum OAuthExceptionType implements RequestExceptionType {
 
-    NOT_SUPPORTED_OAUTH_TYPE("지원 하지 않는 OAuth 서비스 입니다.", HttpStatus.BAD_REQUEST),
+    NOT_SUPPORTED_OAUTH_TYPE("OA01", "지원 하지 않는 OAuth 서비스 입니다.", HttpStatus.BAD_REQUEST),
     ;
 
+    private final String errorCode;
     private final String errorMessage;
     private final HttpStatus httpStatus;
+
+    @Override
+    public String errorCode() {
+        return null;
+    }
 
     @Override
     public String errorMessage() {
--- src/main/java/com/baro/common/exception/CommonRequestExceptionType.java
+++ src/main/java/com/baro/common/exception/CommonRequestExceptionType.java
@@ -6,11 +6,17 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum CommonRequestExceptionType implements RequestExceptionType {
 
-    MISSING_PARAMETER_EXCEPTION("Request parameter is empty", HttpStatus.BAD_REQUEST),
+    MISSING_PARAMETER_EXCEPTION("CM01", "Request parameter is empty", HttpStatus.BAD_REQUEST),
     ;
 
+    private final String errorCode;
     private final String errorMessage;
     private final HttpStatus httpStatus;
+
+    @Override
+    public String errorCode() {
+        return errorCode;
+    }
 
     @Override
     public String errorMessage() {
