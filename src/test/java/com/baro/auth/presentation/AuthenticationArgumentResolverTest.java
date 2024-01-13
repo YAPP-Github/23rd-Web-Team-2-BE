263a4f51745fd67a7d4a1f403002d66e10f8b33c
shinyubin989 <dbqls2821@gmail.com>
2024-01-14 00:44:15 +0900

    test: ArgumentResolver 인수테스트 추가

--- src/test/java/com/baro/auth/presentation/AuthenticationArgumentResolverTest.java
+++ src/test/java/com/baro/auth/presentation/AuthenticationArgumentResolverTest.java
@@ -1,71 +1,46 @@
 package com.baro.auth.presentation;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
+import static com.baro.auth.presentation.ArgumentResolverAcceptanceSteps.Authorization_헤더를_포함한_요청;
+import static com.baro.common.acceptance.AcceptanceSteps.성공;
+import static com.baro.common.acceptance.AcceptanceSteps.응답값을_검증한다;
+import static com.baro.common.acceptance.AcceptanceSteps.응답의_특정_필드값을_검증한다;
+import static io.restassured.RestAssured.given;
 
-import com.baro.auth.application.TokenTranslator;
 import com.baro.auth.domain.AuthMember;
-import io.restassured.RestAssured;
+import com.baro.auth.domain.Token;
+import com.baro.auth.fixture.OAuthMemberInfoFixture;
+import com.baro.common.RestApiTest;
+import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-class AuthenticationArgumentResolverTest {
-
-    @MockBean
-    TokenTranslator tokenTranslator;
+class AuthenticationArgumentResolverTest extends RestApiTest {
 
     @Autowired
     AuthenticationArgumentResolver authenticationArgumentResolver;
 
-    @LocalServerPort
-    int port;
-
-    @BeforeEach
-    void setUp() {
-        RestAssured.port = port;
-    }
-
     @Test
     void 매개변수에_AuthMember가_존재하는경우_Argument_resolver를_거친다() {
         // given
-        String accessToken = "accessToken";
-        Long authMemberId = 1L;
-        setAccessTokenToReturn(authMemberId, accessToken);
+        var 토큰 = 로그인(OAuthMemberInfoFixture.아현());
 
         // when
-        Response response = RestAssured
-                .given()
-                .log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .header("Authorization", accessToken)
-                .get("/test/auth");
+        var 응답 = Authorization_헤더를_포함한_요청(토큰);
 
         // then
-        String result = response.then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract().asString();
-        assertThat(result).isEqualTo(authMemberId.toString());
-    }
-
-    private void setAccessTokenToReturn(Long authMemberId, String accessToken) {
-        given(tokenTranslator.decodeAccessToken(accessToken)).willReturn(authMemberId);
+        응답값을_검증한다(응답, 성공);
+        응답의_특정_필드값을_검증한다(응답, "id", "1");
     }
 }
 
@@ -74,7 +49,24 @@ class AuthenticationArgumentResolverTest {
 class TestController {
 
     @GetMapping("/auth")
-    private Long authUserMethod(AuthMember authMember) {
-        return authMember.id();
+    private ResponseEntity<TestResponse> authUserMethod(AuthMember authMember) {
+        return ResponseEntity.ok().body(new TestResponse(authMember.id()));
+    }
+
+    record TestResponse(Long id) {
+    }
+}
+
+class ArgumentResolverAcceptanceSteps {
+
+    public static ExtractableResponse<Response> Authorization_헤더를_포함한_요청(Token 토큰) {
+        var url = "/test/auth";
+
+        return given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + 토큰.accessToken())
+                .when().get(url)
+                .then().log().all()
+                .extract();
     }
 }
