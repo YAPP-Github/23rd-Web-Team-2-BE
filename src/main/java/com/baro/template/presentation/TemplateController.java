48a7c957634d9d4a013589d10b115d6d2852eb29
shinyubin989 <dbqls2821@gmail.com>
2024-01-15 01:14:55 +0900

    feat: 템플릿 불러오기 api 추가

---
+++ src/main/java/com/baro/template/presentation/TemplateController.java
@@ -0,0 +1,40 @@
+package com.baro.template.presentation;
+
+import com.baro.auth.domain.AuthMember;
+import com.baro.template.application.TemplateService;
+import com.baro.template.application.dto.FindTemplateQuery;
+import com.baro.template.application.dto.FindTemplateResult;
+import com.baro.template.domain.Category;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/templates")
+@RestController
+public class TemplateController {
+
+    private final TemplateService templateService;
+
+    @GetMapping("/{category}")
+    ResponseEntity<List<FindTemplateResult>> findTemplates(
+            AuthMember authMember,
+            @PathVariable("category") String categoryName,
+            @RequestParam("sort") String sortName
+    ) {
+        Sort sort = SortType.getSort(sortName);
+        List<FindTemplateResult> result = templateService.findTemplates(
+                new FindTemplateQuery(Category.getCategory(categoryName), sort));
+
+        if (result.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok().body(result);
+    }
+}
