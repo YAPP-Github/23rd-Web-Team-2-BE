2c2ccd516ead7f5b0910012e35a535b52a1180cf
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-01-16 14:59:13 +0900

    refactor: 예외코드 수정

--- src/main/java/com/baro/memo/exception/TemporalMemoExceptionType.java
+++ src/main/java/com/baro/memo/exception/TemporalMemoExceptionType.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum TemporalMemoExceptionType implements RequestExceptionType {
 
-    NOT_EXIST_TEMPORAL_MEMO("MO02", "존재하지 않는 끄적이는 메모 입니다.", HttpStatus.NOT_FOUND),
-    NOT_MATCH_OWNER("MO03", "끄적이는 메모에 대한 접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
+    NOT_EXIST_TEMPORAL_MEMO("TM02", "존재하지 않는 끄적이는 메모 입니다.", HttpStatus.NOT_FOUND),
+    NOT_MATCH_OWNER("TM03", "끄적이는 메모에 대한 접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
     ;
 
     private final String errorCode;
