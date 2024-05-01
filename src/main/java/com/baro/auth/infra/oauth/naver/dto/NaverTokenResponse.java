57b55abce761f5ddb57b62197e839fd49bd1984a
shinyubin989 <dbqls2821@gmail.com>
2024-05-01 18:08:51 +0900

    hotfix: naver token 요청 응답 객체 Json naming 전략 변경

--- src/main/java/com/baro/auth/infra/oauth/naver/dto/NaverTokenResponse.java
+++ src/main/java/com/baro/auth/infra/oauth/naver/dto/NaverTokenResponse.java
@@ -1,7 +1,10 @@
 package com.baro.auth.infra.oauth.naver.dto;
 
 import com.baro.auth.application.oauth.dto.OAuthTokenInfo;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+@JsonNaming(SnakeCaseStrategy.class)
 public record NaverTokenResponse(
         String accessToken,
         String refreshToken,
